bf81476f79f4f62433044eb4488cca9ca8441142
Alec Kaltschmid <kaltschmidalec@gmail.com>
2022-03-01 20:21:42 -0800

    initial setup

---
+++ src/components/App.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import StreamCreate from "./streams/StreamCreate";
+import StreamDelete from "./streams/StreamDelete";
+import StreamEdit from "./streams/StreamEdit";
+import StreamList from "./streams/StreamList";
+import StreamShow from "./streams/StreamShow";
+import Header from "./Header";
+
+//dont use <a> tags to navigate because they reset your state
+//using <Link> does not reload the page
+
+const App = () => {
+  return (
+    <div className="ui container">
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/new" component={StreamCreate} />
+          <Route path="/streams/edit" component={StreamEdit} />
+          <Route path="/streams/delete" component={StreamDelete} />
+          <Route path="/streams/show" component={StreamShow} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
